349c7237360e9542bf13799cfe44ff3533a69b71
GeorgiiMalishev <malishev292@gmail.com>
2023-04-08 12:59:31 +0500

    first commit

---
+++ Game/Game1.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game;
+
+public class Game1 : Microsoft.Xna.Framework.Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    private Player player = new Player();
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+    }
+    
+    protected override void Update(GameTime gameTime)
+    {
+        var keyPressed = Keyboard.GetState().GetPressedKeys()[0];
+        
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        // TODO: Add your drawing code here
+
+        base.Draw(gameTime);
+    }
+}
---
+++ Game/Player.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game;
+
+public class Player
+{
+    private readonly Texture2D texture;
+    public Vector2 Position;
+    public int Velocity;
+
+    public Player(Texture2D texture, Vector2 position, int velocity)
+    {
+        this.texture = texture;
+        Position = position;
+        Velocity = velocity;
+    }
+
+    public void Update(Keys key)
+    {
+        switch (key)
+        {
+            case Keys.A:
+                Position.X -= Velocity;
+                break;
+            case Keys.D:
+                Position.X += Velocity;
+                break;
+        }
+    }
+    
+    public void Draw(SpriteBatch spriteBatch) => spriteBatch.Draw(texture, Position, Color.White);
+}
